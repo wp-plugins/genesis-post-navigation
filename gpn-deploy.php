00c997e3825293f75437dd12fea0d04ffa7fff20
logeshmba <logeshmba@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-20 08:22:27 +0000

    Version 3.0. Added option to exclude categories and Add squared edges
    git-svn-id: https://plugins.svn.wordpress.org/genesis-post-navigation/tags/3.0@700638 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gpn-deploy.php
@@ -0,0 +1,144 @@
+<?php
+function gpn_custom_css() {
+	// design settings array		
+	$gpn_design	= get_option('gpn-settings');
+	$gpn_bg	= $gpn_design['gpn_bg'];
+	$gpn_bg_hover	= $gpn_design['gpn_bg_hover'];
+	$text_color	= $gpn_design['text_color'];
+	$text_hover	= $gpn_design['text_hover'];
+    $cat_exclude = $gpn_design['gpn_cat_exclude'];		
+	
+	
+// Stores CSS in a string and hooks it in head tag
+$gpn_css = "
+#after-post-nav {
+	height:45px;
+	margin:10px;}
+
+.gps-nav-next{	
+	background: none repeat scroll 0 0 ".$gpn_bg.";
+    border-radius:  0 30px 30px 0;
+   -webkit-border-radius:  0 30px 30px 0;
+   -moz-border-radius:  0 30px 30px 0;
+   -o-border-radius:  0 30px 30px 0;
+    padding: 10px;
+    float: right;                                          
+    margin: 5px 0;
+	display : block; 
+	cursor : pointer;
+	}    
+
+.gps-nav-prev{
+
+	 background: none repeat scroll 0 0 ".$gpn_bg.";
+	 border-radius:30px 0 0 30px;
+	 -moz-border-radius:30px 0 0 30px;
+	 -webkit-border-radius:30px 0 0 30px;
+	 -o-border-radius:30px 0 0 30px;
+	 padding: 10px;
+	 float: left;
+	 margin: 5px 0;
+	 display : block; 
+	 cursor : pointer;
+	 }
+
+.gps-nav-prev a, .gps-nav-next a{
+	     display : block;
+	     color: '.$text_color.' !important;
+	     text-decoration: none;}
+
+.gps-nav-next:hover, .gps-nav-prev:hover{
+	
+		 background: ".$gpn_bg_hover." ;
+		 padding-left:20px;
+		-webkit-transition: all 0.5s ease-in-out;
+		-moz-transition: all 0.5s ease-in-out;
+		-o-transition: all 0.5s ease-in-out;
+		-ms-transition: all 0.5s ease-in-out;}
+
+.gps-nav-prev a:hover{
+        color: ".$text_hover." !important; 
+        padding-left:20px;
+       -webkit-transition: all 0.5s ease-in-out;
+       -moz-transition: all 0.5s ease-in-out;
+       -o-transition: all 0.5s ease-in-out;
+       -ms-transition: all 0.5s ease-in-out;}
+
+.gps-nav-next a:hover{
+	
+        padding-right:20px;
+        color: ".$text_hover." !important; 
+       -webkit-transition: all 0.5s ease-in-out;
+       -moz-transition: all 0.5s ease-in-out;
+       -o-transition: all 0.5s ease-in-out;
+       -ms-transition: all 0.5s ease-in-out;
+        }";
+		
+		
+
+return $gpn_css;
+
+}
+	
+add_action('wp_head','gpn_generate_css');
+
+function gpn_generate_css(){
+	$gpn_design	= get_option('gpn-settings');
+	$gpn_squared_edges = $gpn_design['gpn_squared_edges'];
+	if(is_singular()) {
+	echo '<style>';  	
+	echo gpn_custom_css();  
+	//Adds style for Squared edges 	
+	if($gpn_squared_edges == 1){		
+    echo "
+.gps-nav-next, .gps-nav-prev{
+	border-radius:  0 ;
+	-webkit-border-radius:  0 ;
+	-moz-border-radius:  0 ;
+	-o-border-radius:  0 ;}";}				
+	echo '</style>' ;	
+	}
+}
+
+//Adds Post Navigation Below every single post page	
+
+add_action( 'genesis_after_post_content', 'gpn_after_post' );
+add_action('wp_enqueue_scripts', 'gpn_custom_script');
+
+function gpn_custom_script(){
+ wp_enqueue_script( 'gpn-custom-script', plugins_url( '/js/gpn_custom_script.js', __FILE__ ) );
+}
+
+function gpn_after_post() {
+	$gpn_design	= get_option('gpn-settings');	
+	$cat_nav = $gpn_design['cat_nav'];
+	$postid = get_the_ID();
+	$post_cat =  wp_get_post_categories( $postid ); 
+    $exclude = $gpn_design['gpn_cat_exclude'] ? explode( ',', str_replace( ' ', '', $gpn_design['gpn_cat_exclude'] ) ) : '';
+	//$result_cat = array_diff($post_cat, $exclude);
+   if ( ! is_singular( 'post' ) )
+   return;    
+   if(!in_category($exclude))
+   {
+	  if ( $cat_nav == "1") {?>
+	  <div id="after-post-nav">
+	  <span class="gps-nav-prev">
+	  <?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'gpn') . '</span> %title', true ); ?>
+	  </span>
+	  <span class="gps-nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'gpn`' ) . '
+	  </span>', true ); ?>
+	  </span>       
+      </div><!-- #nav-single -->
+
+      <?php } else {  ?>
+      <div id="after-post-nav">
+      <span class="gps-nav-prev">
+      <?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'gpn') . '</span> %title'); ?></span>
+      <span class="gps-nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'gpn`' ) . '</span>'); ?>
+      </span>
+      </div><!-- #nav-single -->
+	 <?php } 
+   }
+
+}
+
---
+++ plugin.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Genesis Post navigation
+Plugin URI: http://iniyan.in/plugins/genesis-post-navigation/
+Description: Genesis Post navigation plugin adds a previous and next post links on a single post in a conventional way. From the plugin settings page, you can customize the post navigation colors and also it provides an additional option to navigate posts within category. This plugin requires Genesis framework.
+Animations- On mouseover the previous and next links can travel from north pole to south pole :) But, I put just 20px.
+Version: 3.0.0
+Author: Iniyan
+Author URI: http://iniyan.in
+*/
+/** Define our constants */
+define( 'GPN_SETTINGS_FIELD', 'gpn-settings' );
+define( 'GPN_PLUGIN_DIR', dirname( __FILE__ ) );
+register_activation_hook( __FILE__, 'gpn_activation' );
+
+/**
+ * This function runs on plugin activation. It checks to make sure Genesis
+ * or a Genesis child theme is active. If not, it deactivates itself.
+ *
+ * @since 0.1.0
+ */
+function gpn_activation() {
+
+	if ( 'genesis' != basename( TEMPLATEPATH ) ) {
+
+		gpn_deactivate( '1.8.0', '3.3' );
+
+	}
+
+}
+/**
+ * Deactivate GS Design.
+ *
+ *
+ * @since 1.8.0.2
+ */
+
+function gpn_deactivate( $genesis_version = '1.8.0', $wp_version = '3.3' ) {
+
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+
+	wp_die( sprintf( __( 'Sorry, you cannot run Genesis Post Navigation without WordPress %s and <a href="%s">Genesis %s</a> or greater.', 'gsdesign' ), $wp_version, 'http://mobiuztech.com/', $genesis_version ) );
+}
+
+add_action( 'genesis_init', 'gpn_init', 20 );
+
+/**
+ * Load admin menu and helper functions. Hooked to `genesis_init`.
+ *
+ */
+
+function gpn_init() {
+	/** Deactivate if not running Genesis 1.8.0 or greater */
+
+	if ( ! class_exists( 'Genesis_Admin_Boxes' ) )
+
+		add_action( 'admin_init', 'gpn_deactivate', 10, 0 );
+
+	/** Admin Menu */
+
+	if ( is_admin() )
+
+	require_once( GPN_PLUGIN_DIR . '/gpn-design.php' );
+
+	/** CSS generator function */
+
+	require_once( GPN_PLUGIN_DIR . '/gpn-deploy.php' );
+
+}
+
+
+
